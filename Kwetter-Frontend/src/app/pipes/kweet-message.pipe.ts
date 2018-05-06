df7da0bb57b67b5da321d8d32ae779e6ad7a03da
Tim Goes <Tim_Goes4@hotmail.nl>
2018-05-06 12:09:18 +0200

    Hashtags and mentions working with linking in kweets

--- Kwetter-Frontend/src/app/pipes/kweet-message.pipe.ts
+++ Kwetter-Frontend/src/app/pipes/kweet-message.pipe.ts
@@ -7,9 +7,36 @@ import {Kweet} from '../entities/Kweet';
 export class KweetMessagePipe implements PipeTransform {
 
   transform(kweet: Kweet, args?: any): string {
-    if (kweet.text.indexOf('<i>') !== -1) {
-      return kweet.text.replace('<i>', '').replace('<\/i>', '');
+    let message = kweet.text;
+    message = this.generateMentions(kweet, message);
+    message = this.generateTags(kweet, message);
+
+    // TODO: Remove other html tags before generating mentions and hashtags.
+    if (message.indexOf('<i>') !== -1) {
+      return message.replace('<i>', '').replace('<\/i>', '');
     }
   }
 
+  generateMentions(kweet: Kweet, message: string): string {
+    let text = message;
+    for (const mention of kweet.mentions) {
+      text = text.replace(`@${mention.username}`,
+        `<a ng-reflect-router-link="/app-user, ${mention.username}" href="/user/${mention.username}">@${mention.username}</a>`);
+    }
+    return text;
+  }
+
+  generateTags(kweet: Kweet, message: string): string {
+    let lower = message.toLowerCase();
+    let text = message;
+    for (const tag of kweet.tags) {
+      const find = lower.search(`#${tag.tagName}`);
+      const sub = text.slice(find, find + tag.tagName.length + 1);
+      text = text.replace(sub,
+        `<a ng-reflect-router-link="/app-tag-details, ${tag.tagName}" href="/tag/${tag.tagName}">${sub}</a>`);
+      // <a ng-reflect-router-link="/app-tag-details, ${tag.tagName}" href="/tag/${tag.tagName}">#${sub}</a>
+      lower = text.toLowerCase();
+    }
+    return text;
+  }
 }
